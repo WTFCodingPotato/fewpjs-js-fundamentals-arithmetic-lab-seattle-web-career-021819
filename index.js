f444f4317225567cc6fa5c23dcfb74c414a8f76f
Joshua Loran <joshua.loran@gmail.com>
2019-04-10 15:16:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.random() * 100
+
+let mod = 19 % 5
+
+let max = Math.max(1, 2, 5, 7, 8, 13, 17, 20)
